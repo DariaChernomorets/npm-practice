d849d76324f8ebd258dae28330aba68aab46575b
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-08-31 20:41:27 +0300

    First commit

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-'use strict'
+(function() {
 
- const sum = (a,b) => {
-  return a+b;
-}
+    'use strict'
 
-const sub = (a,b) => {
-    return a-b;
-}
+    const sum = (a,b) => {
+        return a+b;
+    }
 
-const multiply = (a,b) => {
-    return a*b;
-}
+    const sub = (a,b) => {
+        return a-b;
+    }
+
+    const multiply = (a,b) => {
+        return a*b;
+    }
 
 
-console.log(sum(1,5));
-console.log(sub(9,15));
-console.log(multiply(8,2));
+    console.log(sum(1,5));
+    console.log(sub(9,15));
+    console.log(multiply(8,2));
 
+
+})();
